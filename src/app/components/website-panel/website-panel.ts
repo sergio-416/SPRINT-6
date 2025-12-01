c741f77e5db94f45f5104d6e5a1c693388a0afc3
Sergio Morey <cmu_416@pm.me>
2025-12-01 12:17:33 +0100

    feat: add WebsitePanel component with HTML, CSS, and unit tests

---
+++ src/app/components/website-panel/website-panel.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-website-panel',
+  imports: [],
+  templateUrl: './website-panel.html',
+  styleUrl: './website-panel.css',
+})
+export class WebsitePanel {
+
+}
